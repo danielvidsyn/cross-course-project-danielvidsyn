f26505dd40ec181e0093b25ae9d91e8e7978bd42
Daniel Wien <64785335+danielvidsyn@users.noreply.github.com>
2023-02-21 12:42:07 +0100

    frontpage finnished

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
+window.addEventListener("load", function() {
+    var toggleBtn = document.querySelector("#toggle");
+    var overlay = document.querySelector("#overlay");
+  
+    if (toggleBtn && overlay) {
+        toggleBtn.addEventListener("click", function() {
+        toggleBtn.classList.toggle("active");
+        overlay.classList.toggle("open");
+      });
+    } else {
+      console.error("Elements not found.");
+    }
+    const shoppingCart = document.querySelector(".shopping-cart-button");
+    const shoppingCartPopup = document.querySelector("#shopping-cart-popup");
+
+    shoppingCart.addEventListener("click", function() {
+      shoppingCartPopup.classList.toggle("open");
+      shoppingCartPopup.classList.toggle("close");
+    });
+});
 
   
