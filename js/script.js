627911fefd08d7035be1c60191258d1abf94fea5
Daniel Wien <64785335+danielvidsyn@users.noreply.github.com>
2023-02-21 15:05:14 +0100

    changed shoppingcart button

--- js/script.js
+++ js/script.js
@@ -10,13 +10,6 @@ window.addEventListener("load", function() {
     } else {
       console.error("Elements not found.");
     }
-    const shoppingCart = document.querySelector(".shopping-cart-button");
-    const shoppingCartPopup = document.querySelector("#shopping-cart-popup");
-
-    shoppingCart.addEventListener("click", function() {
-      shoppingCartPopup.classList.toggle("open");
-      shoppingCartPopup.classList.toggle("close");
-    });
 });
 
   
